db5d123007dcb4361e137fad5adc92f646759c5d
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-05 23:23:20 -0300

    Java Script intermediario, iniciando com numeros aleatorios e If else

---
+++ Random-Number.js
@@ -0,0 +1,24 @@
+//Link de apoio
+//https://la.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+var n = Math.random();
+console.log("numero " + n);
+ 
+
+//Math.random e Math.floor para deixar com numeros inteiros
+var x = Math.random(x);
+x = x * 6
+x = Math.floor(x) + 1;
+console.log("numero " + x);
+
+// Exercio
+// Criar uma calculadora do amor que gere uma porcentagem aleatoria quando digitada dois nomes
+
+
+var name1 = "joao"
+var name2 = "gio"
+
+var love = Math.random(love) 
+love = love * 100
+love = Math.floor(love) + 1
+console.log("sua porcentagem de amor entre " +name1+ " e " +name2+ " é igual á " + love)
