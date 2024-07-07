63d6f66e8bff3fe79330561f1791d3c988490339
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-07 00:08:57 -0300

    Praticando If else

--- If-Else-Conditional&logic.js
+++ If-Else-Conditional&logic.js
@@ -6,7 +6,7 @@
 //  >= maior ou igual a 
 //  <= menor ou igual a
 // && e
-// || 
+// || retorna verdadeiro caso ambos os operandos sejam verdadeiro
 // ! não é igual
 
 
@@ -23,26 +23,28 @@ love = Math.floor(love) + 1;
 
 if(love > 70){
     console.log(
-        "O amor entre " +
-          name1 +
-          " e " +
-          name2 +
-          " é igual á " +
-          love + " é um amor verdadeiro"
-      );
-
+        "O amor entre " +name1 +  "e " +name2 + "é igual á " +love + " é um amor verdadeiro" );
 }if(love > 30 && love <= 70){
     console.log(
-        "sua porcentagem de amor entre " +
-          name1 +
-          " e " +
-          name2 +
-          " é igual á " +
-          love
-      )
-   
-}else{
+        "sua porcentagem de amor entre " + name1 +" e " +name2 +" é igual á " +love)
+   }else{
     console.log("cuidado em ")
 }
 
+// Calculadora Imc com if else
 
+
+var peso = 60;
+var altura = 1.78;
+
+var imc = peso / (altura * altura);
+
+if(imc <= 18.4){
+    console.log("Seu IMC é " +imc + ", então você esta abaixo do peso")
+}
+if(imc >= 18.5 && imc <=24.9){
+    console.log("Seu IMC é " +imc+  ", então você tem um peso normal.")
+}
+else{
+    console.log("Seu IMC é " +imc+  ", então você esta acima do peso.")
+}
