7367429dd839e6e99044a8dbee571a1cf31b462f
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-06 23:43:50 -0300

    Praticando If else

---
+++ If-Else-Conditional&logic.js
@@ -0,0 +1,48 @@
+// Comparadores 
+//  === igual a
+//  !== não é igual a
+//  > maior que
+//  < menor que
+//  >= maior ou igual a 
+//  <= menor ou igual a
+// && e
+// || 
+// ! não é igual
+
+
+ 
+
+
+// Ex de if else
+var name1 = "joao";
+var name2 = "gio";
+
+var love = Math.random(love);
+love = love * 100;
+love = Math.floor(love) + 1;
+
+if(love > 70){
+    console.log(
+        "O amor entre " +
+          name1 +
+          " e " +
+          name2 +
+          " é igual á " +
+          love + " é um amor verdadeiro"
+      );
+
+}if(love > 30 && love <= 70){
+    console.log(
+        "sua porcentagem de amor entre " +
+          name1 +
+          " e " +
+          name2 +
+          " é igual á " +
+          love
+      )
+   
+}else{
+    console.log("cuidado em ")
+}
+
+
