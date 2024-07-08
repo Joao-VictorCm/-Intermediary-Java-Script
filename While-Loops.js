dc850154cbaac037e52712e4cc06f1ebad5d8fa3
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-07-08 18:52:10 -0300

    Loops com while

---
+++ While-Loops.js
@@ -0,0 +1,15 @@
+//Exemplo de Loops com While
+
+
+var numero = 100
+
+function testandowhile(){
+
+    while(numero >= 1){
+        console.log(numero + " garrafas na parede, " + numero + " garrafas de cerveja")
+        numero --
+        console.log("pegue uma e passe de mao em mao " + numero+ " garrafas de cerveja")
+    }
+}
+
+testandowhile()
